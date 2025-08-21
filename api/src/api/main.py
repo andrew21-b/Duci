42ed56cbb7f532d779be525d9deb95426a8fe0c2
Andrew Boateng <andrew.21@outlook.com>
2025-08-21 21:41:28 +0100

    Updates main.py to allow CORS and return static files

--- api/src/api/main.py
+++ api/src/api/main.py
@@ -1,10 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 from src.api.context.config import settings
 from src.api.controller.routes import api_router
 
 app = FastAPI(title=settings.PROJECT_NAME, version=settings.VERSION)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(api_router,prefix=settings.API_V1_STR)
+app.mount("/comparisons", StaticFiles(directory="comparisons"), name="comparisons")
 
 
 @app.get("/")
